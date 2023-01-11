b1c6269e6364038c77e94597a9e19c562b5c42b1
unknown <edernonato@outlook.com>
2023-01-10 21:30:17 -0300

    css selectors fixed

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from selenium import webdriver
 import smtplib
 from datetime import date
 
+from selenium.webdriver.common.by import By
+
 URL = "https://store.epicgames.com/en-US/"
+
+game_text_file = "C:\\Users\\Eder\\PycharmProjects\\new-free-games-epic-games-web-scraping\\games.txt"
+
 
 # SMTP connection
 EMAIL_FROM = "edernonato47teste@hotmail.com"
@@ -33,8 +38,8 @@ free_games_name = [game.text for game in soup.find_all(class_="css-1h2ruwl")]
 free_games_div = soup.find_all(name="div", class_="css-1p2cbqg")
 free_games_link = [[link.get("href") for link in game.find_all(name="a")][1::1] for game in free_games_div][0]
 free_games_date = [[date.text for date in game.find_all(name="span", class_="css-nf3v9d")] for game in free_games_div][0]
-free_games_img = [[image_link.get("data-image") for image_link in game.find_all(name="img", class_="css-1b2k567")] for game in free_games_div][0]
-
+free_games_img = [image.get_attribute("src") for image in driver.find_elements(By.CSS_SELECTOR, ".css-1vu10h2 section .css-1myhtyb img")]
+print(free_games_img)
 html_start = f"""
 <html>
     <head>
@@ -47,23 +52,26 @@ html_end = """
 </html>"""
 
 html_body = ""
+for game in free_games_name:
+    print(game)
 
 for gameIndex in range(len(free_games_name)):
-    # html_body += f"\t\t\t<h4>{free_games_name[gameIndex]}</h4> \n"
+    html_body += f"\t\t\t<h4>{free_games_name[gameIndex]}</h4> \n"
     game_url = URL + free_games_link[gameIndex].replace("/en-US/", "")
     html_body += f"\t\t\t<a href={game_url} style='text-decoration: none; color: #11999E;'><h4>{free_games_name[gameIndex]}</h4></a> \n"
     html_body += f"\t\t\t<img src={free_games_img[gameIndex]}> \n"
     html_body += f"\t\t\t<h5>{free_games_date[gameIndex]} </h5> \n"
 
 html_final = html_start + html_body + html_end
+print(html_final)
 
-with open("games.txt", "r") as old_file:
+with open(game_text_file, "r") as old_file:
     old_html = old_file.read()
     old_file.close()
     if old_html == html_final:
         print("SAME GAMES")
     else:
-        with open("games.txt", "w") as file:
+        with open(game_text_file, "w") as file:
             file.write(html_final)
             print("games.txt updated")
             email_message = MIMEMultipart()
@@ -71,6 +79,5 @@ with open("games.txt", "r") as old_file:
             email_message["to"] = EMAIL_TO
             email_message["subject"] = f"New Free Games Epic Store - {date.today()}"
             email_message.attach(MIMEText(html_final, "html"))
-
             connection.sendmail(from_addr=EMAIL_FROM, to_addrs=EMAIL_TO, msg=email_message.as_string())
             print("New games Alert sent by email!")
